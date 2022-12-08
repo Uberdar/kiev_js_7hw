8a49ae4da97f16dfb880e27fd119f9e6292b9a6c
Alexandr Sanatar <allexvik@gmail.com>
2022-12-08 19:18:36 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector("div.gallery");
+
+function markupMake(obj) {
+  const markup = obj
+    .map(
+      (item) =>
+        `      <img class="gallery__image" src="${item.preview}" alt="${item.description}">`
+    )
+    .join();
+  return markup;
+  //   console.log("markup: ", markup);
+}
+
+galleryEl.insertAdjacentHTML("beforeend", markupMake(galleryItems));
+
+galleryEl.addEventListener("click", galleryClickListener);
+
+function galleryClickListener(e) {
+  console.log(e.target);
+  console.log(e.currentTarget);
+}
+
+// console.log(galleryItems);
