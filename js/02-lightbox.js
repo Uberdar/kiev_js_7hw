5082f76f309a050be3601c1b1e1cad7e9de5c6a4
Alexandr Sanatar <allexvik@gmail.com>
2022-12-09 23:11:30 +0200

    upd 7.2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector("ul.gallery");
+
+const markupMake = galleryItems
+  .map(({ preview, original, description }) => {
+    //   console.log(preview, original, description);
+    const stringMarkup = `<a class="gallery__item" href="${original}">
+  
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>`;
+    return stringMarkup;
+  })
+  .join();
+// console.log("markupMake: ", markupMake);
+
+galleryEl.insertAdjacentHTML("beforeend", markupMake);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  /* options */
+
+  //   caption: true,
+  //   captionType: alt,
+  //   captionsData: alt,
+  //   captionPosition: bottom,
+  showCounter: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+// console.log(galleryItems);
